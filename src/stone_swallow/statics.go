79bedbc1970d0e897a9693c97f6fd40580e1802a
sinmetal <metal.tie@gmail.com>
2017-10-14 20:42:48 +0900

    それとなくDatastoreの統計情報を取得する処理を追加 refs #3

--- src/stone_swallow/statics.go
+++ src/stone_swallow/statics.go
@@ -3,13 +3,46 @@ package stone_swallow
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"golang.org/x/net/context"
 
+	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/runtime"
 	"google.golang.org/appengine/taskqueue"
 )
+
+// StatTotal is Datastoreのすべてのエンティティ
+type StatTotal struct {
+	Count                 int64
+	Bytes                 int64
+	Entity_bytes          int64
+	Builtin_index_bytes   int64
+	Builtin_index_count   int64
+	Composite_index_bytes int64
+	Composite_index_count int64
+	Timestamp             time.Time
+}
+
+func (entity *StatTotal) Load(p []datastore.Property) error {
+	err := datastore.LoadStruct(entity, p)
+	if fmerr, ok := err.(*datastore.ErrFieldMismatch); ok && fmerr != nil && fmerr.Reason == "no such struct field" {
+		// ignore
+	} else if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (entity *StatTotal) Save() ([]datastore.Property, error) {
+	p, err := datastore.SaveStruct(entity)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
 
 func getStatics(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	results := map[string]interface{}{}
@@ -48,6 +81,17 @@ func getStatics(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	log.Infof(ctx, "Runtime.Stats:%s", string(b))
 	results["Runtime.Stats"] = stats
 
+	// Datastore Stats 取得
+	var e []StatTotal
+	_, err = datastore.NewQuery("__Stat_Total__").GetAll(ctx, &e)
+	if err != nil {
+		log.Errorf(ctx, "%s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	results["Datastore.Stats"] = e
+
+	// HTTP ResponseのためのJsonに変換する
 	b, err = json.Marshal(results)
 	if err != nil {
 		log.Errorf(ctx, "%s", err.Error())
