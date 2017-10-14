abec52ba36b6dd4320280cb78f3a570b94f63670
sinmetal <metal.tie@gmail.com>
2017-10-14 19:36:39 +0900

    Runtime.Statsを追加 refs #3

--- src/stone_swallow/statics.go
+++ src/stone_swallow/statics.go
@@ -7,10 +7,14 @@ import (
 	"golang.org/x/net/context"
 
 	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/runtime"
 	"google.golang.org/appengine/taskqueue"
 )
 
 func getStatics(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	results := map[string]interface{}{}
+
+	// Task Queue Stats 取得
 	qsl, err := taskqueue.QueueStats(ctx, []string{"default"})
 	if err != nil {
 		log.Errorf(ctx, "%s", err.Error())
@@ -24,8 +28,32 @@ func getStatics(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	log.Infof(ctx, "TaskQueue.QueueStats:%s", string(b))
+	results["TaskQueue.QueueStats"] = qsl
+
+	// Runtime 取得
+	stats, err := runtime.Stats(ctx)
+	if err != nil {
+		log.Errorf(ctx, "%s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	b, err = json.Marshal(stats)
+	if err != nil {
+		log.Errorf(ctx, "%s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Infof(ctx, "Runtime.Stats:%s", string(b))
+	results["Runtime.Stats"] = stats
+
+	b, err = json.Marshal(results)
+	if err != nil {
+		log.Errorf(ctx, "%s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
